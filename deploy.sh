e6b9b4864330245830e8a9b98bbc0d268cb479bc
Arshi Bhasin <arshi_b@hotmail.com>
2022-07-15 15:10:48 -0400

    ops: moved bulk of deployment script from bash file to yml workspace file

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,11 @@
 
 echo ">>> This is a test file to make it executable."
 
-echo ">>> ENTERING REPO DIRECTORY"
-cd /usr/repos/ && sudo chmod -R 777 huascaran
-cd huascaran
+# echo ">>> PULLING FROM GIT"
+# git pull
 
-echo ">>> PULLING FROM GIT"
-git pull
+# echo ">>> INSTALLING MODULES"
+# pnpm i
 
-echo ">>> INSTALLING MODULES"
-pnpm i
-
-echo ">>> RESTARTING NEXTJS APP"
-pm2 restart nextapp
+# echo ">>> RESTARTING NEXTJS APP"
+# pm2 restart nextapp
