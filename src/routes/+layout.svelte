7da108d4968bf3bb307c923947f6ae50c9672de8
Arshi Bhasin <arshi_b@hotmail.com>
2022-09-28 19:27:36 -0400

    added fade to route changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,17 @@
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '$lib/styles/style.scss';
+	import { fade } from 'svelte/transition';
+
+	export let data;
 </script>
 
 <Header />
 
-<main>
-	<slot />
-</main>
+{#key data.currentRoute}
+	<main in:fade={{ duration: 150, delay: 150 }} out:fade={{ duration: 150 }}>
+		<slot />
+	</main>
+{/key}
+
 <Footer />
