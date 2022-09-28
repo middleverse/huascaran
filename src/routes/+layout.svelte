dc53ef2902213297a75d67a6080c0f4cc45b6ba6
Arshi Bhasin <arshi_b@hotmail.com>
2022-09-28 15:19:07 -0400

    add sass and convert style to scss file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 <script>
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import '$lib/styles/style.css';
+	import '$lib/styles/style.scss';
 </script>
 
 <Header />
