3fadd1316985dbc44a13b324b2d79094079fca84
Arshi Bhasin <arshi_b@hotmail.com>
2022-09-28 17:33:07 -0400

    changed blog content property to component syntax

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	export let data;
+	const { title, date, content: Content } = data;
 </script>
 
 <article>
-	<h1>{data.title}</h1>
-	<p>Published: {data.date}</p>
-	<svelte:component this={data.content} />
+	<h1>{title}</h1>
+	<p>Published: {date}</p>
+	// using content as a component (syntax magic)
+	<Content />
 </article>
