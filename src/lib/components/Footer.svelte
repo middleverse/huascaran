8f8e70abc172b07af07f9230c643bd7952ef16d3
Arshi Bhasin <arshi_b@hotmail.com>
2022-09-29 19:07:18 -0400

    fix footer to bottom of page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,40 +1,21 @@
 <!-- Header.svelte -->
 <footer>
-	<a href="/">Home</a>
-
-	<nav>
-		<ul>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/about">About</a>
-			</li>
-			<li>
-				<a href="/contact">Contact</a>
-			</li>
-		</ul>
-	</nav>
+	<div>© Arshi Bhasin</div>
 </footer>
 
 <style>
 	footer {
+		position: fixed;
+		bottom: 0;
+		width: 100%;
 		padding: 1rem;
-		background: lightskyblue;
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: space-between;
 	}
 
-	ul {
-		margin: 0;
-		list-style-type: none;
-		display: flex;
-		gap: 1rem;
-	}
-
-	a {
-		text-decoration: none;
-		color: inherit;
+	div {
+		margin: auto;
+		text-align: center;
 	}
 </style>
