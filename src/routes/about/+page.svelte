26320ecd93993eba00fa9d7fd8327eb5acc041a6
Arshi Bhasin <arshi_b@hotmail.com>
2022-10-01 12:05:48 -0400

    updated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,7 +1,9 @@
 <!-- about/+page.svelte -->
-<h1 class="text-3xl text-center font-bold pb-3">Mission</h1>
 
-<div class="text-justify">
+<!-- Mission Section -->
+<h1 class="text-3xl text-center font-bold pb-10">Mission</h1>
+
+<div class="text-justify pb-10">
 	<p class="p-3">
 		My hope is for this to be a space the dev community can learn from, get inspired by and
 		influence back.
@@ -10,19 +12,52 @@
 	<p class="p-3">
 		Engineering is tough and <span class="italic">great</span> engineering is without question a
 		constant, hard-fought battle. Most of us have day jobs with taxing deadlines and demanding
-		roadmaps that can sometimes leave us dry, monotonous & potentially questioning the big WHY AM I
-		HERE. To balance out that energy, as engineers, creators, and most importantly problem solvers,
-		we need to make time to
+		roadmaps that can sometimes leave us dry, feeling monotonous & potentially questioning the big
+		WHY AM I HERE. To balance out that energy, as engineers, creators, and most importantly problem
+		solvers, we need to make time to
 		<span class="italic">play</span>.
 	</p>
 
 	<p class="p-3">
-		Play induces a non-judgemental state of creation. Play is a catalyst for happy accidents. Play
-		reminds us to have fun, and so many of us, need this reminder. Play is necessary for growth.
-		Play is a central tenant of longetivity. And I'm looking to do this for a very long time.
+		First and foremost, play reminds us to have fun, and so many of us, need this reminder. But
+		there's more. Play induces a non-judgemental state of creation. Play is a catalyst for happy
+		accidents. Play is necessary for growth. Play is a central tenant of longevity.
 	</p>
+
+	<p class="p-3">And play is missing from our craft.</p>
+
+	<h1 class="logo text-center pb-5 about-signature">Arshi Bhasin</h1>
 
 	<p class="p-3 font-bold">
 		Hi, I'm Arshi. I'm the creator of middleverse and this is my space to play.
 	</p>
+
+	<img
+		alt="profile pic"
+		src="/images/profile.jpg"
+		class="h-48 w-48 container mx-auto m-6 rounded-full border-2 border-rose-400"
+	/>
+
+	<p class="p-3">
+		Currently based in Montreal, Canada. Developing software at
+		<a href="https://livescale.tv" class="underline decoration-4 decoration-rose-400">Livescale</a>.
+		Connect with me on
+
+		<a
+			class="underline decoration-4 decoration-emerald-400"
+			href="https://www.linkedin.com/in/arshibhasin/">LinkedIn</a
+		>
+
+		or
+
+		<a class="underline decoration-4 decoration-emerald-400" href="https://github.com/middleverse"
+			>GitHub</a
+		>.
+	</p>
 </div>
+
+<style>
+	.about-signature {
+		color: rgb(38, 186, 159);
+	}
+</style>
