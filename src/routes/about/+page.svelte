d3e8de7d9c0805e17ec2e6a8df0859d3be25e8a9
Arshi Bhasin <arshi_b@hotmail.com>
2022-10-01 08:25:35 -0400

    completed mission details on the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,28 @@
 <!-- about/+page.svelte -->
-<h1>Hi, I'm Arshi!</h1>
+<h1 class="text-3xl text-center font-bold pb-3">Mission</h1>
 
-<p>This is my about page!</p>
+<div class="text-justify">
+	<p class="p-3">
+		My hope is for this to be a space the dev community can learn from, get inspired by and
+		influence back.
+	</p>
+
+	<p class="p-3">
+		Engineering is tough and <span class="italic">great</span> engineering is without question a
+		constant, hard-fought battle. Most of us have day jobs with taxing deadlines and demanding
+		roadmaps that can sometimes leave us dry, monotonous & potentially questioning the big WHY AM I
+		HERE. To balance out that energy, as engineers, creators, and most importantly problem solvers,
+		we need to make time to
+		<span class="italic">play</span>.
+	</p>
+
+	<p class="p-3">
+		Play induces a non-judgemental state of creation. Play is a catalyst for happy accidents. Play
+		reminds us to have fun, and so many of us, need this reminder. Play is necessary for growth.
+		Play is a central tenant of longetivity. And I'm looking to do this for a very long time.
+	</p>
+
+	<p class="p-3 font-bold">
+		Hi, I'm Arshi. I'm the creator of middleverse and this is my space to play.
+	</p>
+</div>
